8de6be662749b279bfd8eb5b992b0271e9d1e2ca
psychoCS <thiagoluizb@hotmail.com>
2020-11-11 15:46:41 -0500

    Mongo db inserted

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,1 @@
-module.exports = 
-{
-    "URI": "mongodb://localhost/book_store"
-}
+module.exports.URL = "mongodb+srv://TLB2310:tlb2310@cluster0.ldslt.mongodb.net/TLB?retryWrites=true&w=majority";
