60f29cf0cf0a884ff9429df74162bdf0dcd8674f
psychoCS <thiagoluizb@hotmail.com>
2020-11-11 17:28:07 -0500

    created pages

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,11 +2,11 @@ let express = require('express');
 let router = express.Router();
 
 module.exports.displayHomePage = (req, res, next) => {
-    res.render('index', {title: 'Home'});
+    res.render('home', {title: 'Home'});
 }
 
 module.exports.displayAboutPage = (req, res, next) => {
-    res.render('index', { title: 'About'});
+    res.render('about', { title: 'About'});
 }
 
 module.exports.displayProductsPage = (req, res, next) => {
@@ -18,5 +18,5 @@ module.exports.displayServicesPage = (req, res, next) => {
 }
 
 module.exports.displayContactPage = (req, res, next) => {
-    res.render('index', { title: 'Contact'});
+    res.render('contact', { title: 'Contact'});
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,8 @@
 let express = require('express');
 let router = express.Router();
 
+let Survey = require('../models/survey');
+
 let indexController = require('../controllers/index');
 
 /* GET home page. */
