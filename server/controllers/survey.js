b79aaeee2bce4dd830782e276218e9032cfe52e7
dbslapitan <dbslapitan@gmail.com>
2020-11-12 03:20:12 -0500

    CRUD Complete

--- server/controllers/survey.js
+++ server/controllers/survey.js
@@ -15,7 +15,7 @@ module.exports.displaySurveysPage = (req, res, next) => {
 }
 
 module.exports.displayCreatePage = (req, res, next) => {
-    res.render('create-edit', {title: 'Create Survey'});
+    res.render('create-edit', {title: 'Create Survey', surveys: '', buttonName: 'Create'});
 }
 
 module.exports.processCreatePage = (req, res, next) => {
@@ -93,12 +93,49 @@ module.exports.displayEditPage = (req, res, next) => {
           console.log(err);
           res.end();
         }
-        res.render('create-edit', {title:'Edit Survey', surveys:surveyToEdit})
+        res.render('create-edit', {title:'Edit Survey', surveys:surveyToEdit, buttonName: 'Edit'})
       });
 }
 
 module.exports.processEditPage = (req, res, next) => {
-    
+    let id = req.params.id;
+    Survey.updateOne(
+      {_id:id}, 
+      {
+        "_id": id,
+        "title": req.body.surveyTitle,
+        "q1":req.body.question1,
+        "q1o1":req.body.q1choice1,
+        "q1o2":req.body.q1choice2,
+        "q1o3":req.body.q1choice3,
+        "q1o4":req.body.q1choice4,
+        "q2":req.body.question2,
+        "q2o1":req.body.q2choice1,
+        "q2o2":req.body.q2choice2,
+        "q2o3":req.body.q2choice3,
+        "q2o4":req.body.q2choice4,
+        "q3":req.body.question3,
+        "q3o1":req.body.q3choice1,
+        "q3o2":req.body.q3choice2,
+        "q3o3":req.body.q3choice3,
+        "q3o4":req.body.q3choice4,
+        "q4":req.body.question4,
+        "q4o1":req.body.q4choice1,
+        "q4o2":req.body.q4choice2,
+        "q4o3":req.body.q4choice3,
+        "q4o4":req.body.q4choice4,
+        "q5":req.body.question5,
+        "q5o1":req.body.q5choice1,
+        "q5o2":req.body.q5choice2,
+        "q5o3":req.body.q5choice3,
+        "q5o4":req.body.q5choice4
+      },
+      (err) =>
+      {
+        console.log(err);
+        res.end();
+      });
+      res.redirect('/surveys');
 }
 
 module.exports.processDeletePage = (req, res, next) => {
