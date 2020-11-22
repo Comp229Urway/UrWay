5cdae226c6f79e7db3c38306f5ec054a81f7762a
psychoCS <thiagoluizb@hotmail.com>
2020-11-22 16:22:10 -0500

    showing

--- client/src/app/surveys-list/surveys-list.component.ts
+++ client/src/app/surveys-list/surveys-list.component.ts
@@ -10,12 +10,16 @@ import { SurveyRepository } from './../model/survey.repository';
 export class SurveysListComponent
 {
   public selectedSurveyType = null;
+  public SurveysPerPage = 4;
+  public selectedPage = 1;
 
   constructor(private repository: SurveyRepository) { }
 
   get surveys(): Survey[]
   {
-    return this.repository.getSurveys(this.selectedSurveyType);
+    const pageIndex = (this.selectedPage - 1) * this.SurveysPerPage;
+    return this.repository.getSurveys(this.selectedSurveyType)
+      .slice(pageIndex, pageIndex + this.SurveysPerPage);
   }
 
   get surveyTypes(): string[]
@@ -26,5 +30,22 @@ export class SurveysListComponent
   changeSurveyType(newSurvey?: string): void
   {
     this.selectedSurveyType = newSurvey;
+  }
+
+  changePage(newPage: number): void
+  {
+    this.selectedPage = newPage;
+  }
+
+  changePageSize(newSize: number): void
+  {
+    this.SurveysPerPage = Number(newSize);
+    this.changePage(1);
+  }
+
+  get pageNumbers(): number
+  {
+    return Math.ceil(this.repository
+      .getSurveys(this.selectedSurveyType).length / this.SurveysPerPage);
   }
 }
