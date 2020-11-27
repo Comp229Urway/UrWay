2dc4de0d918c884bfa2143e8a4a0ec753f99c4a9
psychoCS <thiagoluizb@hotmail.com>
2020-11-27 16:06:41 -0500

    Log in angular

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -9,11 +9,14 @@ import { SurveyFirstGuards } from './guards/SurveyFirst.guard';
 import { FormsModule } from '@angular/forms';
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent, data:{title:'Home'} },
+  { path: 'home', component: HomeComponent, data: { title: 'Home' } },
+  {path: 'login', data: {title: 'Login'}, redirectTo: '/admin/auth', pathMatch: 'full'},
+
   {path: 'about', component:AboutComponent, data:{title:'About'}  },
   {path: 'contact', component:ContactComponent, data:{title:'Contact'}  },
   { path: 'survey', component: SurveyComponent, data: { title: 'Surveys' } },
   { path: 'survey-list', component: SurveysListComponent, data: { title: 'Surveys List' }, canActivate: [SurveyFirstGuards] },
+
    {path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)},
   {path:'', redirectTo: 'home', pathMatch: 'full'},
   {path:'**', redirectTo: 'home'},
