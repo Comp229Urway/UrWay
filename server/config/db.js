38e2c09c350038c7a6ea9e230dccfe8e644808ae
psychoCS <thiagoluizb@hotmail.com>
2020-12-02 12:00:19 -0500

    Putting on heroku

--- server/config/db.js
+++ server/config/db.js
@@ -1,2 +1,2 @@
-//module.exports.URI = "mongodb+srv://contactlistserver:L@pitan090819@mongodbserver.jmapd.mongodb.net/survey?retryWrites=true&w=majority";
-module.exports.URI = "mongodb://localhost:27017/UrWay";
+module.exports.URI = "mongodb+srv://contactlistserver:L@pitan090819@mongodbserver.jmapd.mongodb.net/survey";
+//module.exports.URI = "mongodb://localhost:27017/UrWay";
