314a15e2e6207b01d6896bfad892d93a3a46e9f6
dbslapitan <dbslapitan@gmail.com>
2020-12-05 00:43:01 -0500

    bcrypt changed to bcryptjs

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,6 +1,6 @@
 let express = require('express');
 const { data } = require('jquery');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 let User = require('../models/users');
 
