db606ac5e2e5d5e5b1b2c8e7ea0c47c82472894b
dbslapitan <dbslapitan@gmail.com>
2020-12-04 23:47:36 -0500

    Site Complete

--- client/src/app/pages/view/view.component.ts
+++ client/src/app/pages/view/view.component.ts
@@ -6,6 +6,10 @@ import { ActivatedRoute, ChildrenOutletContexts, Params, Router } from '@angular
 import { ParticipateModel } from 'src/app/model/participate.model';
 import { SurveyModel } from 'src/app/model/survey.model';
 import { DialogBoxComponent } from 'src/app/partials/dialog-box/dialog-box.component';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+import { SurveysComponent } from '../surveys/surveys.component';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-view',
@@ -20,6 +24,7 @@ export class ViewComponent implements OnInit {
   count : [number[]] = [[]];
   saqTemp: [string[]]= [[]];
   showData = false;
+  toPdf: [{text: string, preserveLeadingSpaces?: boolean}] = [{text: ''}];
   constructor(private route: ActivatedRoute, private http: HttpClient, private dialog: MatDialog, private router: Router) { }
 
   ngOnInit(): void {
@@ -52,19 +57,21 @@ export class ViewComponent implements OnInit {
     {
 
       this.participants = response.participants;
+      this.showData = true;
       this.toNumber();
-      this.getCount()
+      this.getCount();
       console.log(this.templateSurvey);
       console.log(this.count);
       console.log("saqTemp: ", this.saqTemp);
-      this.showData = true;
+      this.generateData();
+
     }
     console.log(response.message);
   });
   }
   getActiveDate()
   {
-    return this.toInputDate(this.templateSurvey.dateActiveStart)+" to "+ this.toInputDate(this.templateSurvey.dateActiveEnd)
+    return this.toInputDate(this.templateSurvey.dateActiveStart)+" to "+ this.toInputDate(this.templateSurvey.dateActiveEnd);
   }
   toInputDate(date: any)
   {
@@ -95,7 +102,6 @@ export class ViewComponent implements OnInit {
     {
       for(let b=0; b < this.participants[a].questionAndAnswer.length; b++) /* Loop through all questions and answers of each participants and compare to survey */
       {
-        let temp: string[] = [];
         if(this.templateSurvey.questionsDetail[b].questionType === "saq")
         {
           this.saqTemp[b].push(this.participants[a]._id + " Answered: " + this.participants[a].questionAndAnswer[b].answer);
@@ -117,5 +123,49 @@ export class ViewComponent implements OnInit {
   {
     let conversion = (data/this.participants.length)* 100;
     return conversion.toFixed(2);
+  }
+  generatePDF()
+  {
+    let docDefinition = {
+      content: [
+        {text: this.templateSurvey.surveyTitle, style: 'title'},
+        {text: '\nDate of Survey: ' + this.toInputDate(this.templateSurvey.dateActiveStart) + ' to ' + this.toInputDate(this.templateSurvey.dateActiveEnd)},
+        {text: '\nDescription: ' + this.templateSurvey.surveyDescription},
+        {text: '\nParticipants: ' + this.participants.length + '\n\n'},
+        this.toPdf
+      ],
+      styles: {
+        title: {
+          fontSize: 22,
+          bold: true,
+          alignment: 'center'
+        }
+      }
+    };
+    pdfMake.createPdf(docDefinition).download(this.templateSurvey.username + '_' + this.templateSurvey.surveyTitle);
+  }
+  generateData()
+  {
+    for(let i=0; i < this.templateSurvey.questionsDetail.length; i++)
+    {
+      this.toPdf.push({text: 'Question ' + (i+1) + ': ' + this.templateSurvey.questionsDetail[i].question});
+
+      if(this.templateSurvey.questionsDetail[i].questionType !== 'saq')
+      {
+        this.toPdf.push({text: '    Options:', preserveLeadingSpaces: true});
+        for(let a = 0; a < this.templateSurvey.questionsDetail[i].choices.length; a++)
+        {
+          this.toPdf.push({text: '        ' + this.templateSurvey.questionsDetail[i].choices[a] + ': ' + this.count[i][a]
+          + ' (' + this.toPercentage(this.count[i][a]) + '%)', preserveLeadingSpaces: true})
+        }
+      }
+      else
+      {
+        for(let b=0; b < this.saqTemp[i].length; b++)
+        {
+          this.toPdf.push({text: '    ' + this.saqTemp[i][b], preserveLeadingSpaces: true})
+        }
+      }
+    }
   }
 }
